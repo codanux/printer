79438bba0ce64daf8d23ea214f7ede1ed2c51e61
codanux <codanux@hotmail.com>
2023-10-19 12:13:15 +0300

    1

---
+++ android/src/main/java/com/codanux/plugin/printer/Printer.java
@@ -0,0 +1,11 @@
+package com.codanux.plugin.printer;
+
+import android.util.Log;
+
+public class Printer {
+
+    public String echo(String value) {
+        Log.i("Echo", value);
+        return value;
+    }
+}
